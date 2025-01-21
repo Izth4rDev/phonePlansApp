a64f0a36718fee0e3a7d1de1d262ca7ffe8b82ed
Izth4rDev <miguel.rondanelli.inf@gmail.com>
2025-01-21 12:08:49 -0300

    test ContractRequest service

--- src/test/java/globant/talentodigital/empresatelefonica/restempresatelefonica/service/imp/ContractRequestServiceTest.java
+++ src/test/java/globant/talentodigital/empresatelefonica/restempresatelefonica/service/imp/ContractRequestServiceTest.java
@@ -1,8 +1,10 @@
 package globant.talentodigital.empresatelefonica.restempresatelefonica.service.imp;
 
+import globant.talentodigital.empresatelefonica.restempresatelefonica.dto.ContractDto;
 import globant.talentodigital.empresatelefonica.restempresatelefonica.dto.ContractRequestDto;
 import globant.talentodigital.empresatelefonica.restempresatelefonica.entity.Contract;
 import globant.talentodigital.empresatelefonica.restempresatelefonica.repository.IContractRepository;
+import globant.talentodigital.empresatelefonica.restempresatelefonica.service.IMapperDTO;
 import globant.talentodigital.empresatelefonica.restempresatelefonica.service.IMapperEntity;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -11,6 +13,8 @@ import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 import java.time.LocalDate;
+import java.util.Arrays;
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.verify;
@@ -25,6 +29,9 @@ class ContractRequestServiceTest {
     @Mock
     private IMapperEntity<Contract, ContractRequestDto> mapperToEntity;
 
+    @Mock
+    //private IMapperDTO<ContractDto, Contract> mapperToDTO;
+    private IMapperDTO<Contract, ContractDto> mapperToDTO;
     @InjectMocks
     private ContractRequestService contractRequestService;
 
@@ -45,11 +52,36 @@ class ContractRequestServiceTest {
         when(mapperToEntity.toEntity(contractRequestDto)).thenReturn(contract);
         when(contractRepository.save(contract)).thenReturn(contract);
 
-
         ContractRequestDto result = contractRequestService.create(contractRequestDto);
 
         assertEquals(contractRequestDto, result);
         verify(contractRepository).save(contract);
         verify(mapperToEntity).toEntity(contractRequestDto);
+    }
+
+    @Test
+    void getAllShouldReturnAllDto(){
+        Contract contract1 = new Contract();
+        contract1.setStartDate(LocalDate.of(2024, 1, 10));
+        contract1.setEndDate(LocalDate.of(2024, 12, 31));
+        Contract contract2 = new Contract();
+        contract2.setStartDate(LocalDate.of(2024, 1, 10));
+        contract2.setEndDate(LocalDate.of(2024, 12, 31));
+        List<Contract> contracts = Arrays.asList(contract1, contract2);
+
+        ContractDto contractDto1 = new ContractDto();
+        contractDto1.setEndDate(LocalDate.of(2024, 1, 10));
+        contractDto1.setStartDate(LocalDate.of(2024, 12, 31));
+        ContractDto contractDto2 = new ContractDto();
+        contractDto2.setEndDate(LocalDate.of(2024, 1, 10));
+        contractDto2.setStartDate(LocalDate.of(2024, 12, 31));
+        List<ContractDto> contractDtos = Arrays.asList(contractDto1, contractDto2);
+
+        when(contractRepository.findAll()).thenReturn(contracts);
+        when(mapperToDTO.toDtos(contracts)).thenReturn(contractDtos);
+
+        List<ContractDto> result = contractRequestService.getAllDtos();
+        assertEquals(contractDtos, result);
+
     }
 }
